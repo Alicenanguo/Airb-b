7fce864596b7ecbbda76f31e72aca6762730938b
Nan <nan_guo@hotmail.com>
2022-09-24 13:49:06 -0700

    update check in users file

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,48 +1,51 @@
-const express = require('express')
+const express = require("express");
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
 const validateSignup = [
-    check('email')
-      .exists({ checkFalsy: true })
-      .isEmail()
-      .withMessage('Please provide a valid email.'),
-    check('username')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-      .not()
-      .isEmail()
-      .withMessage('Username cannot be an email.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
-    handleValidationErrors
-  ];
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be 6 characters or more."),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a valid firstName."),
+  check("firstName")
+    .not()
+    .isEmail()
+    .withMessage("firstName cannot be an email."),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a valid lastName."),
+  check("lastName").not().isEmail().withMessage("lastName cannot be an email."),
+
+  handleValidationErrors,
+];
 
 // Sign up
-router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
+router.post("/", validateSignup, async (req, res) => {
+  const { email, password, username } = req.body;
+  const user = await User.signup({ email, username, password });
 
-      await setTokenCookie(res, user);
+  await setTokenCookie(res, user);
 
-      return res.json({
-        user
-      });
-    }
-  );
-
-
+  return res.json({
+    user,
+  });
+});
 
 module.exports = router;
