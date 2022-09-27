beea8e1108a17685e6b9bfe39aaa8e18a7ff9a9d
Nan <nan_guo@hotmail.com>
2022-09-27 12:25:42 -0700

    update

--- backend/db/migrations/20220926222920-create-spot.js
+++ backend/db/migrations/20220926222920-create-spot.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      OwnerId: {
+      ownerId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
