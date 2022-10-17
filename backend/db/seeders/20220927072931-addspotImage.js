b6f7a6f452095ad9dea57e7c6c8f933d1dace935
Nan <nan_guo@hotmail.com>
2022-10-17 16:38:35 -0700

    all

--- backend/db/seeders/20220927072931-addspotImage.js
+++ backend/db/seeders/20220927072931-addspotImage.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,36 +10,37 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-    await queryInterface.bulkInsert("SpotImages", [
-      {
-
-        spotId: 1,
-        url: "image1 url",
-        preview:true
-      },
-      {
-
-        spotId: 2,
-        url: "image2 url",
-        preview:false
-      },
-      {
-       
-        spotId: 3,
-        url: "image3 url",
-        preview:true
-       }
-     ],{})
+     */
+    await queryInterface.bulkInsert(
+      "SpotImages",
+      [
+        {
+          spotId: 1,
+          url: "https://a0.muscache.com/im/pictures/2e5ce6c9-4935-49ce-891e-e6f7251a8590.jpg?im_w=720",
+          preview: true,
+        },
+        {
+          spotId: 2,
+          url: "https://a0.muscache.com/im/pictures/monet/Luxury-53719772/original/6d19db42-aa39-436d-86fe-8ff36189d84d?im_w=1200",
+          preview: false,
+        },
+        {
+          spotId: 3,
+          url: "https://a0.muscache.com/im/pictures/56467892-0d23-4ab1-8302-9c3d0d5e52cd.jpg?im_w=720",
+          preview: true,
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-     await queryInterface.bulkDelete('SpotImages', null, {})
-  }
+    await queryInterface.bulkDelete("SpotImages", null, {});
+  },
 };
